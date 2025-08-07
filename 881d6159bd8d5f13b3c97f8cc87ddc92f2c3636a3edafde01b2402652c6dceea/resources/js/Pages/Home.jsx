aa14201fffa8f9a5822fcce9195b5d4545be7080
Fittra Marga Ardana <fittracakomoli6@gmail.com>
2025-08-07 09:33:21 +0700

    hero

--- 881d6159bd8d5f13b3c97f8cc87ddc92f2c3636a3edafde01b2402652c6dceea/resources/js/Pages/Home.jsx
+++ 881d6159bd8d5f13b3c97f8cc87ddc92f2c3636a3edafde01b2402652c6dceea/resources/js/Pages/Home.jsx
@@ -392,10 +392,10 @@ export default function Home({
 
                 {/* Konten Teks */}
                 <div className="relative z-10 p-4">
-                    <h1 className="text-3xl md:text-5xl font-bold leading-tight mb-2">
+                    <h1 className="text-3xl md:text-5xl font-bold leading-tight mb-2 md:mb-4">
                         <BlurText text={settings.headline || "Hima Ilkom"} />
                     </h1>
-                    <p className="text-base md:text-xl mb-4 max-w-screen-lg mx-auto">
+                    <p className="text-base md:text-xl mb-4 md:mb-8 max-w-screen-lg mx-auto">
                         <SplitText
                             text={settings.tagline || "Tagline Hima Ilkom"}
                         />
@@ -404,7 +404,7 @@ export default function Home({
                         <AnimatedContent>
                             <Link
                                 href={route("activity.index")}
-                                className="inline-block bg-primary hover:bg-opacity-90 text-white font-semibold py-2 px-4 rounded-lg text-sm transition duration-300 ease-in-out transform hover:scale-105"
+                                className="inline-block bg-primary hover:bg-opacity-90 text-white font-semibold py-2 px-4 md:py-3 md:px-6 rounded-lg text-sm md:text-base transition duration-300 ease-in-out transform hover:scale-105"
                             >
                                 Lebih Dekat
                             </Link>
@@ -412,7 +412,7 @@ export default function Home({
                         <AnimatedContent>
                             <Link
                                 href={route("contact")}
-                                className="inline-block bg-transparent border border-white hover:bg-opacity-90 text-white font-semibold py-2 px-4 rounded-lg text-sm transition duration-300 ease-in-out transform hover:scale-105 hover:bg-secondary"
+                                className="inline-block bg-transparent border border-white hover:bg-opacity-90 text-white font-semibold py-2 px-4 md:py-3 md:px-6 rounded-lg text-sm md:text-base transition duration-300 ease-in-out transform hover:scale-105 hover:bg-secondary"
                             >
                                 Kerja Sama
                             </Link>
